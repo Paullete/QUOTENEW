0dde5690f86142f9649281790e49c9b8637a790e
paullete <paulettenereah3@gmail.com>
2022-04-09 18:48:51 +0300

    errors

--- quote-ip/src/app/app.module.ts
+++ quote-ip/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { DateCountPipe } from './date-count.pipe';
 import { QuoteDetailComponent } from './quote-details/quote-detail/quote-detail.component';
 import { HighlightDirective } from './highlight.directive';
 import { QuoteFormComponent } from './quote-form/quote-form/quote-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { QuoteFormComponent } from './quote-form/quote-form/quote-form.component
     QuoteFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
