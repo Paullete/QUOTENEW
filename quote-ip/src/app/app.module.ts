7827b72a694854846dfda34aaa9752935d6d123a
paullete <paulettenereah3@gmail.com>
2022-04-09 10:56:43 +0300

    quote c

--- quote-ip/src/app/app.module.ts
+++ quote-ip/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote/quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule
