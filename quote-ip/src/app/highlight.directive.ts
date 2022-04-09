cf7c4de1782a1067a1d39f341a1e60272aded897
paullete <paulettenereah3@gmail.com>
2022-04-09 17:25:15 +0300

    changes

--- quote-ip/src/app/highlight.directive.ts
+++ quote-ip/src/app/highlight.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) { }
+  @HostListener("click") maxLikes(){
+    
+    this.quote_highlight("blue")
+  }
+
+
+  private quote_highlight(action:string){
+    this.elem.nativeElement.style.color=action;
+  }
 
 }
